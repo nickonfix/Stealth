fe1ea5f954bcd0c0cc75b2e455d9364aef533794
nickonfix <rjnikhil45@gmail.com>
2026-04-12 17:00:17 +0530

    Added Pagination

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -11,5 +11,8 @@ namespace api.Helpers
         public string? CompnayName { get; set; } = null;
         public string? SortBy { get; set; } = null;
         public bool IsDescending { get; set; } = false;
+
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
     }
 }
