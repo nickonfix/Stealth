7ff3a923a91ebd3ecb2b702ec3371ea69407f081
nickonfix <rjnikhil45@gmail.com>
2026-04-22 18:02:58 +0530

    AuthProf

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -14,7 +14,6 @@ root.render(
   </React.StrictMode>
 );
 
-//console.log(searchCompaines("AAPL"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
