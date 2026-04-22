0a412b4f39898a045f3a27e20e329c155b46636d
nickonfix <rjnikhil45@gmail.com>
2026-04-22 12:53:46 +0530

    deploy it man

--- frontend/src/configs.ts
+++ frontend/src/configs.ts
@@ -1,3 +1,3 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5249/api/";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://localhost:5249/api/";
 
 export const API_URL = BACKEND_URL;
