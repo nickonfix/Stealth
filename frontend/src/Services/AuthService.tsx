48c1cb70be7243d9d5083349c27e6f56fd8d07f0
nickonfix <rjnikhil45@gmail.com>
2026-04-22 13:08:56 +0530

    AuthProf

--- frontend/src/Services/AuthService.tsx
+++ frontend/src/Services/AuthService.tsx
@@ -1,41 +1,15 @@
-// import axios from "axios";
-// import { handleError } from "../Helper/ErrorHandler";
-// import { UserPorfileToken } from "../Models/User"
-// import { API_URL } from "../configs";
-
-// const api = API_URL;
-
-
-// export const loginAPI = async (username: string, password: string)=>{
-//     try{ 
-//         const data = await axios.post<UserPorfileToken>(api + "account/login", {username: username, password:password,});
-//         return data;
-//     }catch(error){
-//         handleError(error);
-//     }
-// }
-
-// export const RegisterAPI = async (username: string, password: string, email: string)=>{
-//     try{ 
-//         const data = await axios.post<UserPorfileToken>(api + "account/register", {username: username, password:password, email:email,});
-//         return data;
-//     }catch(error){
-//         handleError(error);
-//     }
-// }
-   
 import axios from "axios";
 import { handleError } from "../Helper/ErrorHandler";
 import { UserPorfileToken } from "../Models/User";
 import { API_URL } from "../configs";
 
-// Ensure 'api' is treated as the base string
+// The "api" variable from your config
 const api = API_URL;
 
 export const loginAPI = async (username: string, password: string) => {
     try { 
-        // Fixed: Added "/" before account to prevent "apiaccount" concatenation
-        const data = await axios.post<UserPorfileToken>(api + "/account/login", {
+        // We use a template literal to force the slash between the base and the route
+        const data = await axios.post<UserPorfileToken>(`${api}/account/login`, {
             username: username, 
             password: password,
         });
@@ -47,8 +21,8 @@ export const loginAPI = async (username: string, password: string) => {
 
 export const RegisterAPI = async (username: string, password: string, email: string) => {
     try { 
-        // Fixed: Added "/" before account to prevent "apiaccount" concatenation
-        const data = await axios.post<UserPorfileToken>(api + "/account/register", {
+        // We use a template literal to force the slash here too
+        const data = await axios.post<UserPorfileToken>(`${api}/account/register`, {
             username: username, 
             password: password, 
             email: email,
@@ -57,6 +31,4 @@ export const RegisterAPI = async (username: string, password: string, email: str
     } catch(error) {
         handleError(error);
     }
-}   
-
-    
+}
