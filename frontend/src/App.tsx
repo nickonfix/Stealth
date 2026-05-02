e66bff075cbbb0f16d655f5fa0855e4823e55049
nickonfix <rjnikhil45@gmail.com>
2026-05-03 00:18:12 +0530

    Scroll-Fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,35 @@
-
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from 'react-toastify';
-import { UserProvider } from './Context/useAuth';
-function App() {
+import { UserProvider, useAuth } from './Context/useAuth';
 
+const ScrollLock = () => {
+  const { isLoggedIn } = useAuth();
+  
+  useEffect(() => {
+    if (!isLoggedIn()) {
+      document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+      document.documentElement.style.overflow = 'unset';
+    }
+  }, [isLoggedIn()]);
+
+  return null;
+};
+
+function App() {
   return (
-    <>
     <UserProvider>
+      <ScrollLock />
       <Navbar />
       <Outlet />
       <ToastContainer />
     </UserProvider> 
-    </>
   );
 }
 
